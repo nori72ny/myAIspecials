e1e322b56798cd4586de68644e0d5285fe9752c3
nori72ny <nori72ny@gmail.com>
2026-08-28 08:22:47 +0900

    feat: install fail-safe decision memory ingestion

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import "./services/activeContextDecisionIngestion";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
