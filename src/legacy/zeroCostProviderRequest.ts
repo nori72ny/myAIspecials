f959befaf5a68fdaa852b672be3bcaa5678bea4e
nori72ny <nori72ny@gmail.com>
2026-09-05 16:23:39 +0900

    chore: remove unused duplicate zero-cost request helper

--- src/legacy/zeroCostProviderRequest.ts
+++
@@ -1,9 +0,0 @@
-import { ORIGIN_ZERO_COST_OPENROUTER_PROVIDER_POLICY } from "./zeroCostRoutingPolicy";
-
-export function buildZeroCostOpenRouterRequestBody(model: string, messages: unknown[]) {
-  return {
-    model,
-    messages,
-    provider: ORIGIN_ZERO_COST_OPENROUTER_PROVIDER_POLICY,
-  };
-}
